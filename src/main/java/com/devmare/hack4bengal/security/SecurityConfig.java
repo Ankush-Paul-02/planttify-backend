822a7b621b91e6358f64390a00e1d795389bba71
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2024-06-29 16:41:21 +0530

    configure cors

--- src/main/java/com/devmare/hack4bengal/security/SecurityConfig.java
+++ src/main/java/com/devmare/hack4bengal/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,18 +31,20 @@ public class SecurityConfig {
     private final AuthenticationProvider provider;
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("https://hack4bengal-427818.df.r.appspot.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(
-                        cors -> cors.configurationSource(request -> {
-                            CorsConfiguration configuration = new CorsConfiguration();
-                            configuration.setAllowedOrigins(Arrays.asList("*"));
-                            configuration.setAllowedMethods(Arrays.asList("*"));
-                            configuration.setAllowedHeaders(Arrays.asList("*"));
-                            return configuration;
-                        })
-                )
+                .cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         authRequest -> authRequest
                                 .requestMatchers(
