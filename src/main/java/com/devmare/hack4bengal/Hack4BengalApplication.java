efb7be15236b195d26c3929172a731249a67cfa6
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2024-06-29 14:54:08 +0530

    configure cors

--- src/main/java/com/devmare/hack4bengal/Hack4BengalApplication.java
+++ src/main/java/com/devmare/hack4bengal/Hack4BengalApplication.java
@@ -2,11 +2,33 @@ package com.devmare.hack4bengal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Hack4BengalApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Hack4BengalApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://hack4bengal-427818.df.r.appspot.com",
+                                "http://localhost:8001",
+                                "http://localhost:8002",
+                                "http://localhost:8003"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
